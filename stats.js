a7592b148665ea23c1f9c399453b89b23ae5e542
aliklipetz-alt <aliklipetz@gmail.com>
2026-06-19 13:49:44 +0300

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,1 +1,30 @@
+const Stats = {
 
+  saveResult(result) {
+
+    const student = localStorage.getItem("currentStudent");
+
+    if (!student) return;
+
+    const allResults =
+      JSON.parse(localStorage.getItem("lexoraStats") || "[]");
+
+    allResults.push({
+      student,
+      date: new Date().toLocaleString(),
+      ...result
+    });
+
+    localStorage.setItem(
+      "lexoraStats",
+      JSON.stringify(allResults)
+    );
+  },
+
+  getAllResults() {
+    return JSON.parse(
+      localStorage.getItem("lexoraStats") || "[]"
+    );
+  }
+
+};
